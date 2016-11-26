4bcd4650578ec078093d5ad02d0b08a38baa2283
Miguel Bassila <miguelbassila@me.com>
2016-11-26 01:32:26 -0200

    Add ShelfCore

---
+++ ShelfCore/ShelfCore.h
@@ -0,0 +1,19 @@
+//
+//  ShelfCore.h
+//  ShelfCore
+//
+//  Created by Miguel Bassila on 26/11/16.
+//  Copyright © 2016 Miguel Bassila. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ShelfCore.
+FOUNDATION_EXPORT double ShelfCoreVersionNumber;
+
+//! Project version string for ShelfCore.
+FOUNDATION_EXPORT const unsigned char ShelfCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ShelfCore/PublicHeader.h>
+
+
