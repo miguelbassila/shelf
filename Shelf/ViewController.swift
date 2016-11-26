5cf8f6fd5e5ce2ce41ec11437ca082aeca84bbc3
Miguel Bassila <miguelbassila@me.com>
2016-11-26 01:28:49 -0200

    Initial Commit

---
+++ Shelf/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shelf
+//
+//  Created by Miguel Bassila on 26/11/16.
+//  Copyright © 2016 Miguel Bassila. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
